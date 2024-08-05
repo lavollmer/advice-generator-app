990d8e59b8d789efe425cff8f6145b7461283308
Laura <laura.e.vollmer@gmail.com>
2024-08-05 11:30:05 -0500

    updating styling of the advice generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 
+//import images for the project
+import iconDice from "../src/assets/icon-dice.svg";
+import dividerMobile from "../src/assets/pattern-divider-mobile.svg";
+import dividerDesktop from "../src/assets/pattern-divider-desktop.svg";
+
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -36,10 +41,24 @@ function App() {
 
   return (
     <>
-      <div className="bg-dark-blue h-full w-full">
-        <div>
-          <h1>My React App!</h1>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center bg-dark-blue h-full w-full">
+          <div>
+            <h1>Advice #</h1>
+            <div className="flex flex-col items-center justify-center">
+              <pre className="text-neon-green">
+                {JSON.stringify(data, null, 2)}
+              </pre>
+            </div>
+            <div>
+              <img src={dividerMobile} alt="divider mobile" />
+            </div>
+            <div>
+              <button className="bg-neon-green">
+                <img src={iconDice} alt="icon dice" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
